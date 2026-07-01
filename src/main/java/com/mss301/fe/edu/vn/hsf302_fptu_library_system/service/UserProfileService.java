3b67dff0d5585714ba18b4344c1aaef2c250961b
Hieu <nthachhieu@gmail.com>
2026-07-02 02:03:16 +0700

    feat: view profile functionality

---
+++ src/main/java/com/mss301/fe/edu/vn/hsf302_fptu_library_system/service/UserProfileService.java
@@ -0,0 +1,7 @@
+package com.mss301.fe.edu.vn.hsf302_fptu_library_system.service;
+
+import com.mss301.fe.edu.vn.hsf302_fptu_library_system.entity.User;
+
+public interface UserProfileService {
+    User getCurrentUserProfile();
+}
